504fda03dfd2a4d9c1d2e44e99c6082705426fcf
Sergei Zalans <Zalans.serg@gmail.com>
2021-07-29 15:12:33 +0300

    Displayed a copy of the class

--- app/src/main/java/com/example/kotlinlesson/MainActivity.kt
+++ app/src/main/java/com/example/kotlinlesson/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.kotlinlesson
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.kotlinlesson.data.PDCopy
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val copy = PDCopy.pdCopy
+
+        val button = findViewById<Button>(R.id.btn)
+        button.setOnClickListener {
+            val textView = findViewById<TextView>(R.id.textView)
+            textView.text = copy.name + " " + copy.surname + " " + copy.age.toString()
+        }
     }
 }
